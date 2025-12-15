30c048c2b5b1992fdf8d4ced5486818b31c31288
harnishdangi <harnishdangi@gmail.com>
2025-12-15 11:46:36 +0530

    feat: Initialize server with Express, routes, database, and socket.io, including environment variables and project structure documentation.

--- server/src/index.js
+++ server/src/index.js
@@ -29,7 +29,7 @@ app.use("/api/messages", messageRoutes);
 
 app.use(express.static(path.join(__dirname, "../../client/dist")));
 
-app.get("*", (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "../../client/dist", "index.html"));
 });
 
