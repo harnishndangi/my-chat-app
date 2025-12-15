d13c0cff9bb5958fa22285de4624bc92ff157a25
harnishdangi <harnishdangi@gmail.com>
2025-12-15 10:33:28 +0530

    feat: Implement initial server setup with Express, routes, database connection, and socket.io, including package dependencies and scripts.

--- server/src/index.js
+++ server/src/index.js
@@ -14,7 +14,7 @@ app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser());
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["http://localhost:5173", "http://localhost:5174", process.env.CLIENT_URL],
     credentials: true
 }));
 
